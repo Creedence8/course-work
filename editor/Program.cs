77e5e3085aa12dd4516fa88500976a7e519d5260
Vlad K <creedence8@bk.ru>
2016-05-02 23:02:55 +0300

    program.cs

--- editor/Program.cs
+++ editor/Program.cs
@@ -17,6 +17,7 @@ namespace editor
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+			Application.Exception();
             
         }
     }
